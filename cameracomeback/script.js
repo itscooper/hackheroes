4130f59b8f96009074b347c72e0135f33fecaeb9
itscooper <chris.cooper@owasp.org>
2024-10-14 20:32:08 +0100

    Removing unnecessary lines from camera comeback script

--- cameracomeback/script.js
+++ cameracomeback/script.js
@@ -5,10 +5,6 @@ function initRoot(root) {
         root.getElementById('camera2').src = 'cameracomeback/live2.png';
         root.getElementById('camera3').src = 'cameracomeback/live3.png';
         root.getElementById('camera4').src = 'cameracomeback/live4.png';
-        var cameras = root.getElementsByClassName('camera-feed');
-        for (var i = 0; i < cameras.length; i++) {
-        cameras[i].classList.remove('static-feed');
-        }
     });
 }
 function getDetector() {
