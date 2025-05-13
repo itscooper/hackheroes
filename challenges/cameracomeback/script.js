1ea7774a1b723100ded1bfc5db242b9a410c6f01
itscooper <chris.cooper@owasp.org>
2025-05-13 15:38:40 +0100

    New directory structure fixes

--- challenges/cameracomeback/script.js
+++ challenges/cameracomeback/script.js
@@ -1,10 +1,10 @@
 function initRoot(root) {
     root.getElementById('onButton').addEventListener('click', function() {
         console.log('hi2');
-        root.getElementById('camera1').src = 'cameracomeback/live1.png';
-        root.getElementById('camera2').src = 'cameracomeback/live2.png';
-        root.getElementById('camera3').src = 'cameracomeback/live3.png';
-        root.getElementById('camera4').src = 'cameracomeback/live4.png';
+        root.getElementById('camera1').src = 'challenges/cameracomeback/live1.png';
+        root.getElementById('camera2').src = 'challenges/cameracomeback/live2.png';
+        root.getElementById('camera3').src = 'challenges/cameracomeback/live3.png';
+        root.getElementById('camera4').src = 'challenges/cameracomeback/live4.png';
     });
 }
 function getDetector() {
